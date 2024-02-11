6ddbcea01091b979750ca0a43992981817a80725
DINESH Kannaujiya <cat505600@gmail.com>
2024-02-11 23:03:23 +0530

    new fixed some error

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.post('/deploy',async(req,res)=>{
     const repoUrl=req.body.repoUrl;
     const id=generateId();
-   await simpleGit().clone(repoUrl,`output/${id}`);
+    await simpleGit().clone(repoUrl,`output/${id}`);
     await buildProject(id);
 
    const currentModulePath = import.meta.url;
